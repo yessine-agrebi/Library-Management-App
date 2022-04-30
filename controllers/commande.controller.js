5880eae9daf7333782ae9f307d1f3f0ceea485ff
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-05-01 00:46:27 +0200

    controller commande

--- controllers/commande.controller.js
+++ controllers/commande.controller.js
@@ -1,8 +1,13 @@
 import Commande from "../models/commande.model.js";
 
 
-const getCommandes = async (req, res) => {
+export const getCommandes = async (req, res) => {
     try {
-        const cde = await Commande.find()
+        const cde = await Commande.find();
+        res.status(200).json(cde);
+    }catch {
+        res.status(404).json({message: error.message});
     }
 }
+
+
