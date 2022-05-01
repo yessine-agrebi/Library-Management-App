b8af107b53b1b3394f56047d37f1e04e38e9fc0d
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-05-01 22:41:25 +0200

    delete auth

--- controllers/auteur.controller.js
+++ controllers/auteur.controller.js
@@ -20,16 +20,25 @@ export const getOneAuthor = async (req, res) => {
     }
 }
 
-export const updateUser = async (req, res) => {
+export const updateAuthor = async (req, res) => {
     const { id } = req.params;
     
     const { nomauteur, email, numtel} = req.body;
     
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`pas d'auteur avec un id: ${id}`);
 
-    const auth = { nomauteur:nomauteur,email: email, nimtel: numtel, _id: id };
+    const auth = { nomauteur:nomauteur,email: email, numtel: numtel, _id: id };
 
     await Auteur.findByIdAndUpdate(req.params.id, auth);
 
     res.json(auth);
+}
+
+export const deleteAuthor = async (req, res) => {
+    try {
+        const auth = Auteur.findByIdAndRemove(req.params.id);
+        res.status(200).json(auth)
+    }catch(error){
+        res.staus(404).json({message: error.message})
+    }
 }
--- routes/auteurs.route.js
+++ routes/auteurs.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAuteurs, getOneAuthor, updateUser } from "../controllers/auteur.controller.js";
+import { getAuteurs, getOneAuthor, updateAuthor, deleteAuthor } from "../controllers/auteur.controller.js";
 
 
 
@@ -7,7 +7,8 @@ const router = express.Router();
 
 router.get('/', getAuteurs);
 router.get('/:id', getOneAuthor);
-router.put('/:id', updateUser);
+router.put('/:id', updateAuthor);
+router.delete('/:id', deleteAuthor);
 
 
 
