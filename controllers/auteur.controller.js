23f61b79e1275b6fb71f41cfe0c4597e4f2aa4f6
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-05-01 22:34:22 +0200

    update author

--- controllers/auteur.controller.js
+++ controllers/auteur.controller.js
@@ -18,3 +18,17 @@ export const getOneAuthor = async (req, res) => {
         res.status(404).json({message: error.message})
     }
 }
+
+export const updateUser = async (req, res) => {
+    const { id } = req.params;
+    
+    const { nomauteur, email, numtel} = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`pas d'auteur avec un id: ${id}`);
+
+    const auth = { nomauteur:nomauteur,email: email, nimtel: numtel, _id: id };
+
+    await Auteur.findByIdAndUpdate(req.params.id, auth);
+
+    res.json(auth);
+}
