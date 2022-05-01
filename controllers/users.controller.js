a2d1ca2a8411fb84c71778d04099cd869ab4f10b
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-05-01 21:49:31 +0200

    delete user

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -19,3 +19,12 @@ export const getOneUser = async (req, res) => {
         res.status(404).json({message: error.message});
     }
 }
+
+export const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findOneAndRemove(req.params.id);
+        res.status(200).json(user);
+    }catch {
+        res.status(404).json({message: error.message});
+    }
+}
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getOneUser, getUsers } from "../controllers/users.controller.js";
+import { getOneUser, getUsers, deleteUser } from "../controllers/users.controller.js";
 
 const router = express.Router();
 
 router.get('/', getUsers);
 router.get('/:id', getOneUser);
+router.delete('/:id', deleteUser);
+
 
 
 
