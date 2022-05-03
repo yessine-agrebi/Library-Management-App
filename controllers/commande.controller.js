be501f3d7ca259065d83f63bfee0055e149cd441
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-05-03 21:45:45 +0200

    commande crud

--- controllers/commande.controller.js
+++ controllers/commande.controller.js
@@ -1,4 +1,5 @@
 import Commande from "../models/commande.model.js";
+import mongoose from "mongoose";
 
 
 export const getCommandes = async (req, res) => {
@@ -7,6 +8,49 @@ export const getCommandes = async (req, res) => {
         res.status(200).json(cde);
     }catch {
         res.status(404).json({message: error.message});
+    }
+}
+
+export const getOneCommande = async (req, res) => {
+    try {
+        const cmd = await Commande.findById(req.params.id);
+        res.status(200).json(cmd);
+    }catch(error){
+        res.send("not found");
+    }
+}
+
+export const createCommande = async (req, res) => {
+    const commande = req.body;
+    const newCommande = new Commande({...commande});
+    try {
+        await newCommande.save();
+        res.status(201).json(newCommande);
+    }catch(error){
+        res.status(409).json({message: error.message});
+    }
+}
+
+export const updateCommande = async (req, res) => {
+    const { id } = req.params;
+    
+    const commande = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`pas de commande avec un id: ${id}`);
+
+    const cmnd = { ...commande , _id: id };
+
+    await Commande.findByIdAndUpdate(req.params.id, cmnd);
+
+    res.json(cmnd);
+};
+
+export const deleteCommande = async (req, res) => {
+    try {
+        const cmnd = await Commande.findByIdAndRemove(req.params.id);
+        res.status(200).json(cmnd)
+    }catch(error){
+        res.staus(404).json({message: error.message})
     }
 }
 
--- routes/commande.route.js
+++ routes/commande.route.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getCommandes } from "../controllers/commande.controller.js";
+import { getCommandes, getOneCommande, createCommande, updateCommande, deleteCommande } from "../controllers/commande.controller.js";
 
 const router = express.Router();
 
 
 router.get('/', getCommandes);
+router.get('/:id', getOneCommande);
+router.post('/', createCommande);
+router.put('/:id', updateCommande);
+router.delete('/:id', deleteCommande);
 
 
 
