4a40a1e39e783707bcf9e3f172c526a3514c4b40
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-05-03 22:10:03 +0200

    specialite routes

--- routes/specialites.route.js
+++ routes/specialites.route.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { getSpecialites } from "../controllers/specialite.controller.js";
+import { getSpecialites, getOneSpecialite, createSpecialite, updateSpecialite, deleteSpecialite } from "../controllers/specialite.controller.js";
 
 
 const router = express.Router();
 
 
 router.get('/', getSpecialites);
+router.get('/:id', getOneSpecialite);
+router.post('/', createSpecialite);
+router.put('/:id', updateSpecialite);
+router.delete('/:id', deleteSpecialite);
 
 
 export default router;
