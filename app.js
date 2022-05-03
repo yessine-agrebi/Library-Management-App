89e762d6a85489977ea318d3ed8e03656bae576e
ragnar-codes <yessine.agrebi.aiesec@gmail.com>
2022-05-03 23:42:51 +0200

    app.js modif

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-import livreRouter from './routes/livres.route.js';
-import editeurRouter from './routes/editeurs.route.js';
-import auteurRouter from './routes/auteurs.route.js';
-import specialiteRouter from './routes/specialites.route.js';
-import clientRouter from './routes/clients.route.js';
-import commandeRouter from './routes/commande.route.js';
-import userRouter from './routes/users.route.js'
+import livreRouter from "./routes/livres.route.js";
+import editeurRouter from "./routes/editeurs.route.js";
+import auteurRouter from "./routes/auteurs.route.js";
+import specialiteRouter from "./routes/specialites.route.js";
+import clientRouter from "./routes/clients.route.js";
+import commandeRouter from "./routes/commande.route.js";
+import userRouter from "./routes/users.route.js"
 
 dotenv.config();
 const app = express();
@@ -17,24 +17,24 @@ const port = process.env.PORT;
 
 mongoose.connect(db, {
     useNewUrlParser: true,
-}).then(() => console.log('database connected successfully')).catch(err => {console.log('Connection to database rejected', err);
+}).then(() => console.log("database connected successfully")).catch(err => {console.log("Connection to database rejected", err);
 process.exit();
 });
 
 app.use(express.json());
-app.use('/livres', livreRouter);
-app.use('/editeurs', editeurRouter);
-app.use('/auteurs', auteurRouter);
-app.use('/specialites', specialiteRouter);
-app.use('/clients', clientRouter);
-app.use('/commandes', commandeRouter);
-app.use('/users', userRouter);
+app.use("/livres", livreRouter);
+app.use("/editeurs", editeurRouter);
+app.use("/auteurs", auteurRouter);
+app.use("/specialites", specialiteRouter);
+app.use("/clients", clientRouter);
+app.use("/commandes", commandeRouter);
+app.use("/users", userRouter);
 
 
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 })
 
 app.listen(port, () => console.log(`app running on port ${port}`));
